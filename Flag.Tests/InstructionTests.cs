f97a73bc6ef246c5307ee44917482c813de0b28f
David Durschlag <ddurschlag@vistaprint.net>
2017-10-09 14:59:19 -0400

    Add new unit test of basic output using bunting.

--- Flag.Tests/InstructionTests.cs
+++ Flag.Tests/InstructionTests.cs
@@ -12,6 +12,11 @@ namespace Flag.Tests
     using Parse.Instructions;
     using Parse.Tokens;
     using Parse.Structures;
+    using Flag.Compile;
+    using System.IO;
+    using System.CodeDom.Compiler;
+    using Microsoft.CSharp;
+    using System.Reflection;
 
     [TestFixture]
     public class InstructionTests
@@ -19,7 +24,7 @@ namespace Flag.Tests
         [Test]
         public void Render()
         {
-            AssertCompilation(
+            AssertFlagCompilation(
                 "~||~",
                 new Token[] { new FlagToken(), new PoleToken(), new PoleToken(), new FlagToken(), new EndToken() },
                 new Structure[] { new CommandStructure(null, new Structure[0], null) },
@@ -30,7 +35,7 @@ namespace Flag.Tests
         [Test]
         public void Output()
         {
-            AssertCompilation(
+            AssertFlagCompilation(
                 "abc",
                 new Token[] { new StringToken("abc"), new EndToken() },
                 new Structure[] { new OutputStructure("abc") },
@@ -38,8 +43,67 @@ namespace Flag.Tests
             );
         }
 
+        [Test]
+        public void SimpleBunting()
+        {
+            var contents = @"List~
+~
 
-        private static void AssertCompilation(
+~Title|~||~|~:
+~Items|~||Item~|~
+
+~
+
+~Item~
+~  * ~Text|~||~|~
+~
+";
+
+            var sb = new StringBuilder();
+
+            sb.Append(@"
+namespace Testing
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using System.Collections.Generic;
+    class Program
+    {
+        public static string Main()
+        {
+            var sb = new StringBuilder();
+
+            using (var sw = new StringWriter(sb))
+            {
+                NS.CLASS.List(
+                new NS.CLASS.ListViewModel
+                {
+                    Title = ""A"",
+                    Items = new List<NS.CLASS.ItemViewModel> {
+                            ""B"",
+                            ""C""
+                    }
+                },
+                sw
+                );
+            }
+
+            return sb.ToString();
+        }
+    }
+}
+");
+
+            using (var sw = new StringWriter(sb))
+            {
+                new BuntingCompiler(new Compile.CSharp.TemplateCompiler("NS", "CLASS")).Compile(contents, sw);
+            }
+
+            PowerAssert.PAssert.IsTrue(() => CompileAndRun(new string[] { sb.ToString() }) == "\r\n\r\nA:\r\n  * B\r\n  * C\r\n\r\n\r\n");
+        }
+
+        private static void AssertFlagCompilation(
             string s,
             IEnumerable<Token> expectedTokens,
             IEnumerable<Structure> expectedStructures,
@@ -56,6 +120,53 @@ namespace Flag.Tests
                 PowerAssert.PAssert.IsTrue(() => structures.SequenceEqual(expectedStructures, new StructureComparer()));
             if (expectedInstructions != null)
                 PowerAssert.PAssert.IsTrue(() => instructions.SequenceEqual(expectedInstructions, new InstructionComparer()));
+        }
+
+       private static string CompileAndRun(string[] code)
+        {
+            CompilerParameters CompilerParams = new CompilerParameters();
+
+            CompilerParams.GenerateInMemory = true;
+            CompilerParams.TreatWarningsAsErrors = false;
+            CompilerParams.GenerateExecutable = false;
+            CompilerParams.CompilerOptions = "/optimize";
+
+            string[] references = { "System.dll" };
+            CompilerParams.ReferencedAssemblies.AddRange(references);
+
+            CSharpCodeProvider provider = new CSharpCodeProvider();
+            CompilerResults compile = provider.CompileAssemblyFromSource(CompilerParams, code);
+
+            if (compile.Errors.HasErrors)
+            {
+                string text = "Compile error: ";
+                foreach (CompilerError ce in compile.Errors)
+                {
+                    text += "rn" + ce.ToString();
+                }
+                throw new Exception(text);
+            }
+
+            Module module = compile.CompiledAssembly.GetModules()[0];
+            Type mt = null;
+            MethodInfo methInfo = null;
+
+            if (module != null)
+            {
+                mt = module.GetType("Testing.Program");
+            }
+
+            if (mt != null)
+            {
+                methInfo = mt.GetMethod("Main");
+            }
+
+            if (methInfo != null)
+            {
+                return (string)methInfo.Invoke(null, null);
+            }
+
+            return string.Empty;
         }
     }
 }
